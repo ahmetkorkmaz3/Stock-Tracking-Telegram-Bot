3226c3e18537335c02297af95ffdf1ad21c7e6c9
Ahmet <ahmetkorkmaz3453@gmail.com>
2021-02-27 19:14:00 +0300

    get stock price

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 const { Telegraf } = require("telegraf");
 require("dotenv").config();
 const { getStockPrice } = require("./lib/stockRequest");
-const { commandArgs } = require("./lib/commandArgs");
+const commandArgs = require("./lib/commandArgs");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.use(commandArgs())
 
-/*
 bot.start((ctx) => {
-  ctx.reply(`Hello ${ctx.from.first_name}, you can list your stock list`);
+  ctx.reply(
+    `
+    Selam ${ctx.from.first_name}, bu botu kullanarak hisse senedi fiyatının anlık değerini takip edebilirsiniz.
+    /help komutu ile nasıl çalıştığını öğrenebilirsiniz.
+
+    Geliştirici: Murat Ahmet KORKMAZ
+    Katkı sağlamak için: https://github.com/ahmetkorkmaz3
+    `
+    );
 });
 
- */
-bot.command('price', ctx => {
-    console.log(ctx.state.command);
+bot.command('stock', async ctx => {
+  try {
+    const data = await getStockPrice(ctx.state.command.arg.toUpperCase());
+
+    ctx.reply(
+      `${ctx.state.command.arg.toUpperCase()} Hissesi:
+      Alış Fiyatı: ${data.data.hisseYuzeysel.alis} ₺
+      Satış Fiyatı: ${data.data.hisseYuzeysel.satis} ₺
+      Tavan Fiyat: ${data.data.hisseYuzeysel.tavan} ₺
+      Taban Fiyat: ${data.data.hisseYuzeysel.taban} ₺
+      `
+    )
+
+  } catch (error) {
+    ctx.reply(error.error)
+  }
 })
+
+
 bot.launch();
 
 process.once("SIGINT", () => bot.stop("SIGINT"));
