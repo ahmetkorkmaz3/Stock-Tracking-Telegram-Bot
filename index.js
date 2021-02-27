909f823d95ffb8ed5a430ab7f17579654274c396
Ahmet <ahmetkorkmaz3453@gmail.com>
2021-02-27 20:38:19 +0300

    altın, dolar, euro add

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { Telegraf } = require("telegraf");
 require("dotenv").config();
-const { getStockPrice } = require("./lib/stockRequest");
+const { getStockPrice, getDovizPrice } = require("./lib/requests");
 const commandArgs = require("./lib/commandArgs");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.use(commandArgs())
+bot.use(commandArgs());
 
 bot.start((ctx) => {
   ctx.reply(
@@ -16,10 +16,20 @@ bot.start((ctx) => {
     Geliştirici: Murat Ahmet KORKMAZ
     Katkı sağlamak için: https://github.com/ahmetkorkmaz3
     `
-    );
+  );
 });
 
-bot.command('stock', async ctx => {
+bot.help((ctx) => {
+  ctx.reply(
+    `
+    Merhaba ${ctx.from.first_name}, aşağıdaki komutları kullanarak bilgi edinebilirsin.
+    /hisse SYMBOL_ADI => bu komutu kullanarak hisse senedinin anlık değerini öğrenebilirsin.
+    /doviz komutunu kullanarak altın, dolar, euro fiyatlarını öğrenebilirsin.
+    `
+  );
+});
+
+bot.command("hisse", async (ctx) => {
   try {
     const data = await getStockPrice(ctx.state.command.arg.toUpperCase());
 
@@ -30,13 +40,53 @@ bot.command('stock', async ctx => {
       Tavan Fiyat: ${data.data.hisseYuzeysel.tavan} ₺
       Taban Fiyat: ${data.data.hisseYuzeysel.taban} ₺
       `
+    );
+  } catch (error) {
+    ctx.reply(error.error);
+  }
+});
+
+bot.command("doviz", async (ctx) => {
+  try {
+    const data = await getDovizPrice();
+
+    const gold = data.data.filter(item => {
+      return item.SEMBOLID == 2199
+    })
+
+    const euro = data.data.filter(item => {
+      return item.SEMBOLID == 1639
+    });
+
+    const dolar = data.data.filter(item => {
+      return item.SEMBOLID == 1302
+    })
+
+    ctx.reply(
+      `Altın Kuru:
+      Alış Fiyatı: ${gold[0].ALIS} ₺
+      Satış Fiyatı: ${gold[0].SATIS} ₺
+      `
+    );
+
+    ctx.reply(
+      `Euro Kuru:
+      Alış Fiyatı: ${euro[0].ALIS} ₺
+      Satış Fiyatı: ${euro[0].SATIS} ₺
+      `
     )
 
-  } catch (error) {
-    ctx.reply(error.error)
+    ctx.reply(
+      `Dolar Kuru:
+      Alış Fiyatı: ${dolar[0].ALIS} ₺
+      Satış Fiyatı: ${dolar[0].SATIS} ₺
+      `
+    )
+
+  } catch (err) {
+    console.log(err);
   }
 })
-
 
 bot.launch();
 
